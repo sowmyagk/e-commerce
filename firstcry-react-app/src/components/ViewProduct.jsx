6bf3c04ec489b8fae7fa77b16b495146eeeaede3
sowmyagk <sowmya.ds22@sahyadri.edu.in>
2026-04-16 12:56:09 +0530

    Initial commit

--- firstcry-react-app/src/components/ViewProduct.jsx
+++ firstcry-react-app/src/components/ViewProduct.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./viewproduct.css";
+import "./Viewproduct.css";
 
 function ViewProduct() {
   const [products, setProducts] = useState([]);
