803318a5edca80f426d9217b61f9a2416bac650a
sowmyagk <sowmya.ds22@sahyadri.edu.in>
2026-04-27 00:00:58 +0530

    updated codes

--- firstcry-react-app/src/CreateAccount.jsx
+++ firstcry-react-app/src/CreateAccount.jsx
@@ -1,4 +1,5 @@
-import { useState, useLocation, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import "./CreateAccount.css";
 
 function CreateAccount() {
