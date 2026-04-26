1a1ad5a5f29211792621269d3cd47b2d71cf46dc
sowmyagk <sowmya.ds22@sahyadri.edu.in>
2026-04-26 17:43:10 +0530

    updated codes

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,26 +1,26 @@
 const nodemailer = require("nodemailer");
 
 const sendEmailOTP = async (email, otp) => {
-  try {
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
 
+  try {
     await transporter.sendMail({
       from: process.env.EMAIL_USER,
       to: email,
-      subject: "Your OTP Code",
+      subject: "OTP Verification",
       text: `Your OTP is ${otp}`,
     });
 
-    console.log("✅ Email sent");
-  } catch (error) {
-    console.log("❌ Email error:", error);
-    throw error;
+    console.log("✅ OTP Email Sent");
+  } catch (err) {
+    console.log("❌ EMAIL ERROR:", err);
+    throw err;
   }
 };
 
