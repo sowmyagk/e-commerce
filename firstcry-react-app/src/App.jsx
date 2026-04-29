c984efc565ea569081e88c782a421a1d28846e90
sowmyagk <sowmya.ds22@sahyadri.edu.in>
2026-04-29 09:21:30 +0530

    updated codes

--- firstcry-react-app/src/App.jsx
+++ firstcry-react-app/src/App.jsx
@@ -15,42 +15,26 @@ import Checkout from "./pages/Checkout";
 import Payment from "./pages/Payment";
 import Success from "./pages/Success";
 
-function Layout({ children }) {
-  return (
-    <>
-      <Header />
-      {children}
-    </>
-  );
-}
-
 function App() {
   return (
     <Routes>
-
-      <Route path="/" element={<Layout><div /></Layout>} />
-      <Route path="/cart" element={<Layout><Cart /></Layout>} />
-      <Route path="/wishlist" element={<Layout><Wishlist /></Layout>} />
-      <Route path="/product/:id" element={<Layout><ProductDetails /></Layout>} />
-      <Route path="/orders" element={<Layout><OrderHistory /></Layout>} />
-      <Route path="/checkout" element={<Layout><Checkout /></Layout>} />
-      <Route path="/payment" element={<Layout><Payment /></Layout>} />
-      <Route path="/success" element={<Layout><Success /></Layout>} />
-
-  
+      <Route path="/" element={<Header />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<CreateAccount />} />
-      <Route path="/OtpPage" element={<OtpPage />} />
-
-      
       <Route path="/adminLogin" element={<AdminLogin />} />
+      <Route path="/OtpPage" element={<OtpPage />} />
       <Route path="/admindashboard" element={<AdminDashboard />} />
       <Route path="/viewproduct" element={<ViewProduct />} />
       <Route path="/addproduct" element={<AddProduct />} />
       <Route path="/edit-product/:id" element={<AddProduct />} />
-
-      <Route path="/cancel" element={<h2>Payment Cancelled</h2>} />
-
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/wishlist" element={<Wishlist />} />
+      <Route path="/product/:id" element={<ProductDetails />} />
+      <Route path="/orders" element={<OrderHistory />} />
+      <Route path="/checkout" element={<Checkout />} />
+      <Route path="/payment" element={<Payment />} />
+      <Route path="/success" element={<Success />} />
+      <Route path="/cancel" element={<h2>Payment Cancelled </h2>} />
     </Routes>
   );
 }
