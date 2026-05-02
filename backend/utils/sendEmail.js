4b1b12c4f4eedc3bcbcb0bf607566e2bb376a924
sowmyagk <sowmya.ds22@sahyadri.edu.in>
2026-05-02 20:52:59 +0530

    updated codes

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,30 +1,22 @@
-import nodemailer from "nodemailer";
+const nodemailer = require("nodemailer");
 
 const sendEmail = async (to, otp) => {
-  try {
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-      }
-    });
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
 
-    const mailOptions = {
-      from: process.env.EMAIL_USER,
-      to: to,
-      subject: "Your OTP Code",
-      text: `Your OTP is: ${otp}`
-    };
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: to,
+    subject: "Your OTP Code",
+    text: `Your OTP is ${otp}`,
+  };
 
-    const info = await transporter.sendMail(mailOptions);
-
-    console.log("EMAIL SENT:", info.response);
-
-  } catch (err) {
-    console.log("EMAIL ERROR:", err.message);
-    throw err;
-  }
+  await transporter.sendMail(mailOptions);
 };
 
-export default sendEmail;
+module.exports = sendEmail;
