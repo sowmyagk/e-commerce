34fb949470ffe4e11f4bfa72ad544f6f650dd56a
sowmyagk <sowmya.ds22@sahyadri.edu.in>
2026-05-03 10:05:19 +0530

    updated codes

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -2,9 +2,7 @@ const nodemailer = require("nodemailer");
 
 async function sendEmail(to, otp) {
   const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,          // ✅ CHANGE THIS
-    secure: false,      // ✅ MUST be false for 587
+    service: "gmail", // ✅ use this instead of host
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
